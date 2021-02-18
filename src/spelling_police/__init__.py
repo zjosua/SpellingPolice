603b00decaadb7fe3adb6815da2dff45880db7ab
lovac42 <lovac42@protonmail.com>
2021-02-18 18:23:29 -0500

    added credit section for code contributors

--- src/spelling_police/__init__.py
+++ src/spelling_police/__init__.py
@@ -3,6 +3,9 @@
 # Support: https://github.com/lovac42/SpellingPolice
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
+# Code Contributors:
+#   (2020) dgeranton - added word suggestion list
+
 
 #from .lib.com.lovac42.anki.others import safety_first
 
