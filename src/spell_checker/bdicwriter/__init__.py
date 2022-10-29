91f25bd1e6fa83a5de80568332f5a1b581bc7976
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-10-29 13:01:49 +0900

    separate __main__ file

--- src/spell_checker/bdicwriter/__init__.py
+++ src/spell_checker/bdicwriter/__init__.py
@@ -81,26 +81,3 @@ def create_bdic(words: List[str], aff: Optional[str] = None) -> bytes:
     dic_bytes(words, output)
     output[md5_start:data_start] = hashlib.md5(output[data_start:]).digest()
     return bytes(output)
-
-
-# For testing purposes
-if __name__ == "__main__":
-    import sys
-    from pathlib import Path
-
-    input_dic = Path(sys.argv[1])
-    input_aff = input_dic.parent / (input_dic.stem + ".aff")
-    output_bdic = Path(sys.argv[2])
-    dic_file = input_dic.read_text()
-    lines = dic_file.split("\n")[1:]  # first line contains word count
-    words = list(filter(lambda i: i, map(lambda line: line.strip(), lines)))
-
-    aff_string: Optional[str] = None
-    if input_aff.exists():
-        print("Using input aff")
-        aff_string = input_aff.read_text()
-    else:
-        print("Using default aff")
-
-    b = create_bdic(words, aff=aff_string)
-    output_bdic.write_bytes(b)
